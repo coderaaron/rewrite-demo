f256ccffa83aeb1434e35d1575cdcad4479ad27c
Aaron Graham <aegraham@gmail.com>
2013-12-09 16:39:09 -0600

    add rewrite tag so WP can use variable from URL

--- rewrite-demo.php
+++ rewrite-demo.php
@@ -6,15 +6,26 @@
  */
  
 function ag_add_rewrite_rule() {
-	add_rewrite_rule( 'howdy', 'index.php?p=1', 'top');
-	
+	add_rewrite_tag('%referrer%','([^&]+)');
+
 	add_rewrite_rule(
-		'^([^/]*)/referal/^([^/]*)/?$', 
-		'index.php?p=$matches[1]&referal=$matches[2]',
+		'^([^/]+)/referal/([^/]+)', 
+		'index.php?p=$matches[1]&referrer=$matches[2]',
 		'top'
 	);
+
+	add_rewrite_rule( 'howdy', 'index.php?p=1', 'top');
 
 	flush_rewrite_rules();
 }
 add_action( 'init', 'ag_add_rewrite_rule');
+
+function ag_content_filter($content) {
+	if (get_query_var( 'referrer' )) {
+		return $content."<br>referred by:".get_query_var( 'referrer' );
+	}
+	return $content;
+}
+
+add_filter( 'the_content', 'ag_content_filter' );
 ?>
